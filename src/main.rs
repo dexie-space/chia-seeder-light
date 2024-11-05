105da3b40308965135f92b6bd8b5be42780a17e4
dennis <dns@dexie.space>
2024-11-05 02:41:52 +0100

    avoid logging request ips by default

--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,10 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_target(false)
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                tracing_subscriber::EnvFilter::new("info")
+                    .add_directive("trust_dns_server=warn".parse().unwrap())
+            }),
         )
         .init();
 
