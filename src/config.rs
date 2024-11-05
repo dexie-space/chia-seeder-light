b2443c8f44faa9f2e9de8d01cbc9dc4d90e07277
dennis <dns@dexie.space>
2024-11-05 11:19:32 +0100

    tune recheck interval

--- src/config.rs
+++ src/config.rs
@@ -4,5 +4,5 @@ pub const PEER_TIMEOUT: u64 = 10;
 pub const DNS_RECORD_TTL: u32 = 300;
 pub const MAX_CONCURRENT_TASKS: usize = 100;
 pub const MAX_RECORDS_TO_RETURN: usize = 32;
-pub const RECHECK_INTERVAL: Duration = Duration::from_secs(1800);
+pub const RECHECK_INTERVAL: Duration = Duration::from_secs(3600);
 pub const PEER_BLOCKLIST_TTL: u64 = 86400;
